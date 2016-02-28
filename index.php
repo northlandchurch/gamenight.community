787a53eb1ba6ee1017a36031d761c5213785541a
Robert J <greeneagle@gmail.com>
2016-02-28 12:10:34 -0500

    More work.
    Removed the “atnorthland” link. Dumb idea. Added more stuff.

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://use.fonticons.com/ffd19869.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-inverse">
@@ -22,13 +23,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Game Night</a>
+          <a class="navbar-brand" href="#"><i class="fa fa-home"></i> Game Night</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
-            <li><a href="atnorthland/">at Northland</a></li>
-            <li><a href="http://orlandogamers.net">OrlandoGamers.net (Forums)</a></li>
+            <li><a href="events/"><i class="fa fa-calendar"></i> Events</a></li>
+            <li><a href="http://orlandogamers.net"><i class="fa fa-comments-o"></i> Forums</a></li>
           </ul>
         </div>
       </div>
@@ -37,22 +38,75 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-6">
+          <!-- <img src="assets/gn_logo.png" class="img-responsive" alt="" /> -->
           <h1 class="media-heading">Let's Play</h1>
-            <p class="lead">
+            <p class="lead text-primary">
               Our hope is to help you get connected to other gamers, board gaming clubs,
               sport leagues and video game tournaments.
             </p>
+            <h2>Gaming is good for you.</h2>
             <p>
               Truth is, it can be hard to connect to people these days.
               Whether it's through sports, board games or even video games, playing
               games allow you to hang out together with friends and family, creating
               deeper bonds of friendship.
             </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
         </div>
         <div class="col-sm-6">
-          <p>
-            text-justify
-          </p>
+
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Featured Events</h3>
+            </div>
+            <div class="panel-body">
+              <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                    <img src="assets/IMG_0235.jpg" alt="...">
+                    <div class="carousel-caption">
+                      Superfun!
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="assets/IMG_0290.jpg" alt="...">
+                    <div class="carousel-caption">
+                      Morun!
+                    </div>
+                  </div>
+                    <div class="item">
+                      <img src="assets/IMG_0236.jpg" alt="...">
+                      <div class="carousel-caption">
+                        Some games being played.
+                    </div>
+                  </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+
         </div>
       </div>
 
